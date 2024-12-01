45bfab4b2c0886064e961e207110639ab3e386c4
Living-Hell <s.living.hell@gmail.com>
2024-12-01 21:49:12 +0530

    Refactored signup page

---
+++ frontend/src/app/components/signup/signup.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
+})
+export class SignupComponent {
+  username: string = '';
+  password: string = '';
+
+  onSubmit() {
+    // Implement your login logic here
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    // Add authentication logic and navigate to the next page upon successful login
+  }
+}
