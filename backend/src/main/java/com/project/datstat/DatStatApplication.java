2c9c51886b9d4ce151d81be54e33ecc399ab8c74
Living-Hell <s.living.hell@gmail.com>
2024-12-01 14:45:59 +0530

    Updated folder structure

---
+++ backend/src/main/java/com/project/datstat/DatStatApplication.java
@@ -0,0 +1,13 @@
+package com.project.datstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatStatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatStatApplication.class, args);
+    }
+
+}
