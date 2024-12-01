09eab02af582bdca8194c28d282b508096da31b1
sidhanthanwatework <sidhant.hanwate@datoms.io>
2024-12-01 19:38:56 +0530

    added signup page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< HEAD
 import { LoginPageComponent } from './components/login-page/login-page.component';
+=======
+import { SignupComponent } from './signup/signup.component';
+>>>>>>> 7cb973a (added signup page)
 
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< HEAD
   imports: [RouterOutlet, LoginPageComponent],
+=======
+  imports: [RouterOutlet, SignupComponent],
+>>>>>>> 7cb973a (added signup page)
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 import {LoginPageComponent} from "./components/login-page/login-page.component";
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
-  {path: "login", component: LoginPageComponent}];
+  {path: "login", component: LoginPageComponent},
+  { path: 'signup', component: SignupComponent },
+  { path: '', component: SignupComponent },
+];
