8ccf697541388fe2ba58106c8c6af0a5af09be78
Living-Hell <s.living.hell@gmail.com>
2024-12-01 13:36:51 +0530

    Adding spring backend

---
+++ backend/DatStat/src/main/java/com/project/DatStat/DatStatApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.project.DatStat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatStatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatStatApplication.class, args);
+    }
+
+}
---
+++ backend/DatStat/src/test/java/com/example/DatStat/DatStatTests.java
@@ -0,0 +1,13 @@
+package com.example.DatStat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatStatTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
